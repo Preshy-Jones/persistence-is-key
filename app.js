a8059b14792f824f67cf8bea23efb5c5a7d55c5c
Preshy-Jones <adedibuprecious@gmail.com>
2020-05-18 15:07:56 +0100

    updated files

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const express = require('express');
 const app = express()
 const fs = require('fs');
+const port = process.env.PORT || 8001
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/delete', (req, res) => {
 
-    fs.unlink('myjsonfile.json', function (err) {
-        if (err) throw err;
-        // if no error, file has been deleted successfully
-        res.send('deleted successfully')
-    });
+    fs.access('myjsonfile.json', fs.F_OK, (err) => {
+        if (err) {
+            res.send('no words to delete')
+        } else {
+            fs.unlink('myjsonfile.json', function (err) {
+                if (err) throw err;
+                // if no error, file has been deleted successfully
+                res.send('deleted successfully')
+            });
+
+        }
+    })
     // fs.writeFile('myjsonfile.json', '', function () { console.log('done') })
     // res.send('all content deleted')
 });
 
 app.get('/api/list', (req, res) => {
     res.header("Content-Type", "text/plain");
-    fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data) {
+    fs.access('myjsonfile.json', fs.F_OK, (err) => {
         if (err) {
-            console.log(err);
+            res.send('no words added yet')
         } else {
-            obj = JSON.parse(data); //now it an object
+            fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    obj = JSON.parse(data); //now it an object
 
-            res.end(obj.words.toString())
+                    res.end(obj.words.toString())
+                }
+            });
+
         }
-    });
+    })
 });
+
+
 
 
 app.get('/api/:something', (req, res) => {
@@ -54,23 +72,15 @@ app.get('/api/:something', (req, res) => {
                     fs.writeFile('myjsonfile.json', json, 'utf8', err => console.log(err)); // write it back 
                     res.send('word added successfully')
                 }
-            });    // data = req.params.something;
+            });
 
         }
     })
-    // fs.appendFile('words.txt', data, function (err) {
-    //     if (err) throw err;
-    //     res.send('success')
-    //});
-    // fs.writeFile('words.txt', data, err => console.log(err));
-    // res.send('success')
-    // db.push(req.params.something); 
-    //res.json({ msg: db })
+
 });
 
 
 
-app.listen(3000, function () {
-    console.log("started server on port 3000");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
-
